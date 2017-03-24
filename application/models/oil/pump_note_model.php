eff5175108e4f4d05c413c63e716693308f5ae8e
littlejietu <littlejietu@qq.com>
2017-03-24 16:49:56 +0800

    new

---
+++ application/models/oil/pump_note_model.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pump_note_model extends XT_Model {
+
+	protected $mTable = 'oil_pump_note';
+	
+}
--- application/models/oil/site_config_model.php
+++ application/models/oil/site_config_model.php
@@ -20,6 +20,7 @@ class Site_config_model extends XT_Model {
 				}else{
 					$arrSub = array('wx_appid'=>$info['wx_appid'],'wx_mchid'=>$info['wx_mchid'],
 							'wx_key'=>$info['wx_key'],'wx_appsecret'=>$info['wx_appsecret'],
+							'wx_auth_refresh_token'=>$info['wx_auth_refresh_token'],
 							'wx_token'=>$info['wx_token'],'site_id'=>$site_id, 'company_id'=>$company_id,
 							'ali_appid'=>$info['ali_appid'],'ali_auth_token'=>$info['ali_auth_token'],
 						);
